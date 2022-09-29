52b2d5b32ee290c09d75544c1d2a7dbf77f52a9b
kjsong99 <mine7118@kpu.ac.kr>
2022-09-29 22:21:51 +0900

    글 수정, 삭제 action sheet

--- Move/ViewController/PostViewController.swift
+++ Move/ViewController/PostViewController.swift
@@ -22,7 +22,31 @@ class PostViewController: UIViewController {
             contentLabel.sizeToFit()
         }
         
-//        contentLabel.text = String(id)
+        //        contentLabel.text = String(id)
+    }
+    
+    
+    @IBAction func showMoreButton(_ sender: Any) {
+        let actionSheet = UIAlertController(title: "글메뉴", message: nil, preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "수정", style: .default, handler: {(ACTION:UIAlertAction) in
+            modifyPost()
+            print("수정되었습니다.")
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "삭제", style: .default, handler: {(ACTION:UIAlertAction) in
+            deletePost()
+            print("삭제되었습니다.")
+        }))
+        
+        //취소 버튼 - 스타일(cancel)
+        actionSheet.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+        
+        
+        
+        
     }
     
     func getPost(id: Int) async throws{
@@ -30,7 +54,7 @@ class PostViewController: UIViewController {
         
         do{
             let data = try await AppNetworking.shared.requestJSON(url, type: PostsResponseElement.self, method: .get)
-                    
+            
             titleLabel.text = data.title
             contentLabel.text = data.content
             userLabel.text = data.user?.username
@@ -39,18 +63,20 @@ class PostViewController: UIViewController {
             guard let date = data.created_at.dateUTC(format: "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") else{
                 return
             }
-             let str = date.string(format: "MM/dd HH:mm")
+            let str = date.string(format: "MM/dd HH:mm")
             dateLabel.text = str
-           
+            
             
         }
-        
-        
-        
-        
-        
     }
     
+}
+
+func modifyPost(){
+    
+}
+
+func deletePost(){
     
 }
 
